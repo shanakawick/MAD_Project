582e745a81884efc3b62255acbaf7bd8143793c6
@HiruniMalaviarachchi <mail2hiruni@gmail.com>
2019-09-26 08:05:56 +0530

    Pictures have been edited

---
+++ app/src/main/java/com/example/kitchenstories/upload_Receipe.java
@@ -0,0 +1,14 @@
+package com.example.kitchenstories;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class upload_Receipe extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload__receipe);
+    }
+}
